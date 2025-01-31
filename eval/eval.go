2369cf1c52b8699681466c7dd7a9a0bff2f40cea
aiden-lee11 <aidentlee11@gmail.com>
2025-01-31 01:07:04 -0600

    basic structure

--- eval/eval.go
+++ eval/eval.go
@@ -2,14 +2,30 @@ package eval
 
 import (
 	"fmt"
+	"sort"
 )
 
 // Card will be a byte with vals | cdhsrrrr |
 type Card byte
+type HandStrength int
+
+const (
+	RoyalFlush HandStrength = iota
+	StraighFlush
+	Quads
+	FullHouse
+	Flush
+	Straight
+	Trips
+	TwoPair
+	OnePair
+	HighCard
+)
 
 // func for generating combos, evaluating a single 5 card hand strength, evaluating best 5 card hand out 7
 
 // Takes in 7 cards and returns all possible 5 card hands, essentially 7 chose 5
+// Thanks cesar :)
 func GenerateCombinations(cards []Card) [][]Card {
 	if len(cards) != 7 {
 		fmt.Println("Expected 7 cards in the combo generator got: ", len(cards))
@@ -34,4 +50,49 @@ func GenerateCombinations(cards []Card) [][]Card {
 
 	helper(0, []Card{})
 	return combos
+}
+
+type Hand []Card
+
+func (hand *Hand) EvaluateHand() HandStrength {
+	sort.Slice(hand, func(i, j int) bool {
+		return (*hand)[i]&0x0F < (*hand)[j]&0x0F
+	})
+
+	if hand.isFlush() {
+		if !hand.isStraight() {
+			return Flush
+		}
+
+		if (*hand)[0]&0x09 != 0 {
+			return RoyalFlush
+		}
+		return StraighFlush
+	}
+
+	if hand.isQuads() {
+		return Quads
+	}
+
+	if hand.isFullHouse() {
+		return FullHouse
+	}
+
+	if hand.isStraight() {
+		return Straight
+	}
+
+	if hand.isTrips() {
+		return Trips
+	}
+
+	if hand.isTwoPair() {
+		return TwoPair
+	}
+
+	if hand.isOnePair() {
+		return OnePair
+	}
+
+	return HighCard
 }
